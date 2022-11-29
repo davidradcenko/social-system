9d4d82d95f860bfa0de9b7dbdbe12d7b4751fae1
David Radchenko <davidradchenko2@gmail.com>
2022-11-29 05:50:10 -0800

    added munu

--- src/elements/Account/Account.tsx
+++ src/elements/Account/Account.tsx
@@ -3,6 +3,8 @@ import {Navigate} from "react-router-dom";
 import React from "react";
 import {RootState, useAppDispatch} from "../../store/store";
 import {useSelector} from "react-redux";
+import testimg from "../../img/imgTest.jpeg.jpg"
+import "./Account.css"
 
 export const Account = () => {
     const dispatch = useAppDispatch()
@@ -16,15 +18,15 @@ export const Account = () => {
         <div className={"MainBlocCoverAccount"}>
             <div className={"MenuAccount"}>
                 <div className="imgFrofil">
-                    <img src="../../img/imgTest.jpeg.jpg" alt="Imadge whis main foto"/>
+                    <img src={testimg} alt="Imadge whis main foto"/>
                 </div>
                 <div className="InfoUserInMenu">
                     <p className={"NameUserMenu"}>David Radchenko</p>
-                    <p className={"StutusWorkUserMenu"}>Status Work: I looking</p>
+                    <p className={"StutusWorkUserMenu"}><span className={"status-work"}>Status Work:</span> I looking</p>
                 </div>
                 <div className="linksMenu">
                     <ul>
-                        <li><a href="">My profile</a></li>
+                        <li><a className={"InSelect"} href="">My profile</a></li>
                         <li><a href="">Messages</a></li>
                         <li><a href="">Users in app</a></li>
                     </ul>
