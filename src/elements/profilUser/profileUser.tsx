6bd3cf5881646469d43e7e6277fd11a0b908bfb7
David Radchenko <davidradchenko2@gmail.com>
2023-01-17 04:25:39 -0800

    end to make navigation div

--- src/elements/profilUser/profileUser.tsx
+++ src/elements/profilUser/profileUser.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./profileUser.css";
 import ImgLogo from "../../img/common.png";
 
@@ -6,10 +6,18 @@ import HomeIcon from "../../img/nav-icons/home.png";
 import DialogsIcon from "../../img/nav-icons/Frame.png";
 import MesengerIcon from "../../img/nav-icons/heart.png";
 import SeachIcon from "../../img/nav-icons/Group 43.png";
-import SetingsIcon from "../../img/nav-icons/settings.png.png";
+import SetingsIcon from "../../img/nav-icons/settings.png";
+
+import imgVector from "../../img/nav-icons/Vector 1.jpg";
 
 export const ProfileUser = () => {
+    const [TextPapap, SetTextPapap] = useState(false)
+    const [Vector, SetVector] = useState(true)
 
+    const changeSettextPapap = () => {
+        SetTextPapap(!TextPapap)
+        SetVector(!Vector)
+    }
     return (
         <div className={"Main-block"}>
             <div className={"Navigation-block"}>
@@ -22,19 +30,32 @@ export const ProfileUser = () => {
                         </div>
                     </div>
                     <div className="main-navi">
-                        <div className={"ProfilIcon"}>
+                            <div className={"ProfilIcon"}>
                             <img src={HomeIcon} alt="HomeIcon"/>
                             <div className={"ProfilIcon-text"}>
                                 <p>Profil</p>
-                                <p>Edit profile</p>
+                                {Vector==true? <img className={"VectorTrue"} onClick={changeSettextPapap} src={imgVector} alt="imgVector"/> : <img className={"VectorFals"} onClick={changeSettextPapap} src={imgVector} alt="imgVector"/> }
+                                {TextPapap == true ? <p className={"ProfileIcpn-Test-p-true"}>Edit profile</p> :
+                                    <p className={"ProfileIcpn-Test-p-false"}>Edit profile</p>}
                             </div>
                         </div>
 
-
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                        <div className={"AlseIcons"}>
+                            <img src={DialogsIcon} alt="DialogsIcon"/>
+                            <p>Dialogs</p>
+                        </div>
+                        <div className={"AlseIcons"}>
+                            <img src={MesengerIcon} alt="MesengerIcon"/>
+                            <p>Message</p>
+                        </div>
+                        <div className={"AlseIcons"}>
+                            <img src={SeachIcon} alt="SeachIcon"/>
+                            <p>Seach</p>
+                        </div>
+                        <div className={"AlseIcons"}>
+                            <img src={SetingsIcon} alt="SetingsIcon"/>
+                            <p>Settings</p>
+                        </div>
                     </div>
                 </div>
             </div>
