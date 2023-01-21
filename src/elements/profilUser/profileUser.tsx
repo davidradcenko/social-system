31dd51d7b9ac8b2b095a40704f234a236b97f979
David Radchenko <davidradchenko2@gmail.com>
2023-01-21 05:47:12 -0800

    start make edit profil mode

--- src/elements/profilUser/profileUser.tsx
+++ src/elements/profilUser/profileUser.tsx
@@ -16,6 +16,8 @@ import facebook from "../../img/icons-profel/Social-3.png";
 import vk from "../../img/icons-profel/Social-1.png";
 import vaiber from "../../img/icons-profel/Social-2.png";
 
+import VectopCloseEditWindow from "../../img/Vector-close-Edit-profil.png";
+
 import customization from "../../img/icons-profel/customizachen.png";
 
 import testInfoBlockImg from "../../img/icons-profel/Ellipse 17.png";
@@ -24,9 +26,23 @@ import {isBooleanObject} from "util/types";
 export const ProfileUser = () => {
     const [TextPapap, SetTextPapap] = useState(false)
     const [Vector, SetVector] = useState(true)
+    const changeSettextPapap = () => {
+        SetTextPapap(!TextPapap)
+        SetVector(!Vector)
+    }
+
+
     const [PapapSelection, SetPapapSelection] = useState(false)
     const [CheckboxStat, SetCheckboxStat] = useState<boolean>()
+    const [PapapSelection2, SetPapapSelection2] = useState(false)
+    const [CheckboxStat2, SetCheckboxStat2] = useState<boolean>()
 
+    const changeSetCheckboxStat2=(valer:boolean)=>{
+        SetCheckboxStat2(valer)
+    }
+    const changeSetPapapSelection2= ()=>{
+        SetPapapSelection2(!PapapSelection2)
+    }
     const changeSetCheckboxStat=(valer:boolean)=>{
         SetCheckboxStat(valer)
     }
@@ -34,11 +50,10 @@ export const ProfileUser = () => {
         SetPapapSelection(!PapapSelection)
     }
 
-    const changeSettextPapap = () => {
-        SetTextPapap(!TextPapap)
-        SetVector(!Vector)
+    const [EditModeProfil, SetEditModeProfil] = useState<boolean>(false)
+    const changeSetEditModeProfil=()=>{
+        SetEditModeProfil(!EditModeProfil)
     }
-
 
     return (
         <div className={"Main-block"}>
@@ -61,7 +76,7 @@ export const ProfileUser = () => {
                                          alt="imgVector"/> :
                                     <img className={"VectorFals"} onClick={changeSettextPapap} src={imgVector}
                                          alt="imgVector"/>}
-                                {TextPapap == true ? <p className={"ProfileIcpn-Test-p-true"}>Edit profile</p> :
+                                {TextPapap == true ? <p onClick={changeSetEditModeProfil} className={EditModeProfil==true?"ProfileIcpn-Test-p-true BlackOnCliked":"ProfileIcpn-Test-p-true"}>Edit profile</p> :
                                     <p className={"ProfileIcpn-Test-p-false"}>Edit profile</p>}
                             </div>
                         </div>
@@ -86,10 +101,24 @@ export const ProfileUser = () => {
                 </div>
             </div>
             <div className={"Main-content"}>
+                <div className={EditModeProfil==true?"Edit-profil-menu":"Edit-profil-menu-none"}>
+                    <img onClick={changeSetEditModeProfil} className={"VectopCloseEditWindow"} src={VectopCloseEditWindow} alt="VectopCloseEditWindow"/>
+                    <div className="Edit-mode-Frofil">
+                        <p>Edit profile</p>
+                        <div className="EditMode-classfotoFrofel-gradient"><img src={testForo} alt="testForo"/></div>
+
+                        <div className="Edit-mode-Inputs">
+                            <p></p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className={EditModeProfil==true?"boground-Shadow":"boground-Shadow-none" }></div>
                 <div className="mainChaend">
                     <div className="main-info">
                         <div className={"lardge-foto"}>
-                            <img src={testForo} alt="large img"/>
+                            <div className="Foto-classfotoFrofel-gradient"><img src={testForo} alt="large img"/></div>
                         </div>
                         <div className={"info-profil"}>
                             <p className="Name-InfoProfil">DAVID RADCHENKO</p>
@@ -119,6 +148,7 @@ export const ProfileUser = () => {
                             <input className={"seatch-menu"} type="text" placeholder={"Seach"}/>
                             <img onClick={changeSetPapapSelection} src={customization} alt="customization"/>
                             {/*className={"one-pass-customisazia"}*/}
+
                              <div className={PapapSelection== true? "one-pass-customisazia" : "one-pass-customisazia-none"} >
                                 <p className={"name-custom"}>Sort by:</p>
                                 <div onBlur={changeSetPapapSelection} className="change-customizat">
@@ -128,14 +158,19 @@ export const ProfileUser = () => {
                             </div>
                         </div>
                         <div className={"two-pas"}>
-                            <img src={customization} alt="customization"/>
+                            <img onClick={changeSetPapapSelection2} src={customization} alt="customization"/>
                             <p className="recomend-menu"> Recommendations</p>
+
+                            <div className={PapapSelection2== true? "two-pass-customisazia" : "two-pass-customisazia-none"} >
+                                <p className={"name-custom"}>Sort by:</p>
+                                <div onBlur={changeSetPapapSelection2} className="change-customizat">
+                                    <p className={CheckboxStat2==true? "Customizasia-Status":"Customizasia-Status-NoCheked"}>Status   <input className={"Status-button"} checked={CheckboxStat2==true} onChange={()=>changeSetCheckboxStat2(true)} name={"radio-customez2"}  type="radio"/></p>
+                                    <p className={CheckboxStat2==false?"Customizasia-Profesion-Skills-Cheked":"Customizasia-Profesion-Skills"}>Professional skills<input className={"Profesion-Skils-button"} checked={CheckboxStat2==false} onChange={()=>changeSetCheckboxStat2(false)} name={"radio-customez2"}   type="radio"/></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-
                 </div>
-
                 <div className={"content-functional"}>
                     {/*one-pass*/}
                     <div className={"one-pass-block-content"}>
@@ -244,7 +279,7 @@ export const ProfileUser = () => {
                                 </div>
                             </div>
                             <div className={"two-pas-function"}>
-                                <input type="button" value={"Delete"}/>
+                                <input type="button" value={"Subscribe"}/>
                             </div>
                         </div>
                         <div className="two-pass-info-block">
@@ -259,7 +294,7 @@ export const ProfileUser = () => {
                                 </div>
                             </div>
                             <div className={"two-pas-function"}>
-                                <input type="button" value={"Delete"}/>
+                                <input type="button" value={"Subscribe"}/>
                             </div>
                         </div>
                         <div className="two-pass-info-block">
@@ -274,7 +309,7 @@ export const ProfileUser = () => {
                                 </div>
                             </div>
                             <div className={"two-pas-function"}>
-                                <input type="button" value={"Delete"}/>
+                                <input type="button" value={"Subscribe"}/>
                             </div>
                         </div>
                         <div className="two-pass-info-block">
@@ -289,7 +324,7 @@ export const ProfileUser = () => {
                                 </div>
                             </div>
                             <div className={"two-pas-function"}>
-                                <input type="button" value={"Delete"}/>
+                                <input type="button" value={"Subscribe"}/>
                             </div>
                         </div>
                         <div className="two-pass-info-block">
@@ -304,7 +339,7 @@ export const ProfileUser = () => {
                                 </div>
                             </div>
                             <div className={"two-pas-function"}>
-                                <input type="button" value={"Delete"}/>
+                                <input type="button" value={"Subscribe"}/>
                             </div>
                         </div>
                         <div className="two-pass-info-block">
@@ -319,7 +354,7 @@ export const ProfileUser = () => {
                                 </div>
                             </div>
                             <div className={"two-pas-function"}>
-                                <input type="button" value={"Delete"}/>
+                                <input type="button" value={"Subscribe"}/>
                             </div>
                         </div>
                     </div>
